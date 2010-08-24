2ef365e00d5fff7ec2cc5c645ece1e769ec97a5f
Cal Henderson <cal@iamcal.com>
2010-08-24 15:57:08 -0500

    delete all the old optimized files first, so we can generated partial results while it's running (it takes aaaaages now)

--- optimize.sh
+++ optimize.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 rm -f perl_images_optipng/*.png
+rm -f perl_images_pngcrush/*.png
+rm -f perl_images_pngout/*.png
+
 optipng -zc1-9 -zm1-9 -zs0-3 -f0-5 -dir perl_images_optipng perl_images/*.png
 
-rm -f perl_images_pngcrush/*.png
 pngcrush -brute -d perl_images_pngcrush/ perl_images/*
 
 perl pngout.pl
